b22a2eb0e24f345d02f63135ba2fe70ea20a2dbb
Subrato Kumar Verma <138965935+18subrato@users.noreply.github.com>
2025-03-02 23:25:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-//your JS code here. If required.
+document.getElementById('userForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    let userName = document.getElementById('name').value;
+    let userAge = document.getElementById('age').value;
+    
+    if (!userName || !userAge) {
+        alert("Please enter valid details.");
+        return;
+    }
+
+    new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (userAge > 18) {
+                resolve(`Welcome, ${userName}. You can vote.`);
+            } else {
+                reject(`Oh sorry ${userName}. You aren't old enough.`);
+            }
+        }, 4000);
+    })
+    .then(message => alert(message))
+    .catch(message => alert(message));
+});
